54c8271490d4d8311515bd1c79a56ac927d5c1f3
jlmcabral <1140471@isep.ipp.pt>
2021-06-15 09:18:19 +0100

    Add axios to perform HTTP request to google's API

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = process.env.GOOGLE_API;
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-
 function searchAddressHandler(event: Event) {
     event.preventDefault();
-    const enteredAddress = addressInput.value;
+        const enteredAddress = addressInput.value;
 
-    // send to google's API
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${API_KEY}`)
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 }
 form.addEventListener('submit', searchAddressHandler);
